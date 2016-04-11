f59d7db87061eba26ac6393375f3b3b819282e5f
OscarV2 <ing.odvega@gmail.com>
2016-04-11 01:23:56 -0500

    Commit Main

--- app/src/main/java/com/example/admin_sena/myambulaciaparamedico/MapsActivity.java
+++ app/src/main/java/com/example/admin_sena/myambulaciaparamedico/MapsActivity.java
@@ -1,9 +1,16 @@
 package com.example.admin_sena.myambulaciaparamedico;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -23,6 +30,16 @@ import java.net.URL;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    //Variable para guardar la posicion inicial del equipo
+    private Location posicionActual = null;
+    private LocationManager locationMangaer = null;
+    private LocationListener locationListener = null;
+    String MejorProveedor = null;
+    //Variable que controla el tiempo en que se actualiza la ubicacion en segundos
+    private static int TIEMPO_ACTUALIZACION = 15;
+    //Variable que controla la actualizacion del radio de movimiento de la ambulancia en metros
+    private static int RADIO_ACTUALIZACION = 7;
+    Context cnt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +48,95 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
+        cnt=this;
+        locationMangaer = (LocationManager) getSystemService(cnt.LOCATION_SERVICE);
+        //this to set delegate/listener back to this class
+
+        Criteria req = new Criteria();
+        req.setAccuracy(Criteria.ACCURACY_FINE);
+        req.setAltitudeRequired(true);
+
+        //Mejor proveedor por criterio
+        MejorProveedor = locationMangaer.getBestProvider(req, false);
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+        }
+        //Configuramos el listener para que verifique la ubicaciones cada 10000 milisegundos y 20 metros, si cumple las dos condiciones
+        //se dispara el metodo
+        locationListener = new MiUbicacion();
+        locationMangaer.requestLocationUpdates(MejorProveedor, TIEMPO_ACTUALIZACION, RADIO_ACTUALIZACION, locationListener);
+
+
         mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        boolean network_enabled = locationMangaer.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
+        if (network_enabled) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+            }
+            Location location = locationMangaer.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            if(location!=null){
+                posicionActual = location;
+                CrearMarcador(location, "Mi Ubicación");
+            }
+        }
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+       // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public void CrearMarcador(Location location, String Titulo)
+    {
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(location.getLatitude(), location.getLongitude()))
+                .title(Titulo));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 15.0f));
+
+
+
     }
 
 
+
+    private class MiUbicacion implements LocationListener
+    {
+
+        @Override
+        public void onLocationChanged(Location location) {
+CrearMarcador(location,"Mi Ubicacion");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
 
 
 }
